7f240744ba3848f3fcfe822ce8bae1e4d605588e
Abhishek SP <abhisp@thoughtworks.com>
2016-12-28 00:14:06 +0530

    add test for five disks

--- towerofhanoi_test.go
+++ towerofhanoi_test.go
@@ -31,3 +31,16 @@ func TestSolveShouldSolveTwoDiskProblem(t *testing.T) {
 	assert.Equal(t, pegB, []int{})
 	assert.Equal(t, pegC, []int{2, 1})
 }
+
+func TestSolveShouldSolveFiveDiskProblem(t *testing.T) {
+	pegA := []int{5, 4, 3, 2, 1}
+	pegB := []int{}
+	pegC := []int{}
+	n := 5
+
+	solve(n, &pegA, &pegB, &pegC)
+
+	assert.Equal(t, pegA, []int{})
+	assert.Equal(t, pegB, []int{})
+	assert.Equal(t, pegC, []int{5, 4, 3, 2, 1})
+}
